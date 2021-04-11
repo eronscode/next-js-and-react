c064eabddf62c4cd465646034dfed02ab1285301
Dev_Ose <osemum@hardcorebiometric.com>
2021-04-11 15:23:08 +0100

    Added a custom 404 error page

---
+++ pages/404.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function NotFoundPage(props) {
+    return (
+        <div>
+            <h2>The page you're looking for cannot be found</h2>
+        </div>
+    )
+}
+
+export default NotFoundPage
+
