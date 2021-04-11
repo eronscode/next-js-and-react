9d4c30256a34548e3c79d141cf1f87f18815270f
Dev_Ose <osemum@hardcorebiometric.com>
2021-04-11 13:33:14 +0100

    Removed redundant files + added first page

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,17 @@
-import { Fragment } from 'react';
-import Head from 'next/head';
+import React from 'react'
+import PropTypes from 'prop-types'
 
-import FeaturedPosts from '../components/home-page/featured-posts';
-import Hero from '../components/home-page/hero';
-import { getFeaturedPosts } from '../lib/posts-util';
-
-function HomePage(props) {
+function index(props) {
   return (
-    <Fragment>
-      <Head>
-        <title>Max' Blog</title>
-        <meta
-          name='description'
-          content='I post about programming and web development.'
-        />
-      </Head>
-      <Hero />
-      <FeaturedPosts posts={props.posts} />
-    </Fragment>
-  );
+    <div>
+      <h1>Homepage</h1>
+    </div>
+  )
 }
 
-export function getStaticProps() {
-  const featuredPosts = getFeaturedPosts();
+index.propTypes = {
 
-  return {
-    props: {
-      posts: featuredPosts,
-    },
-  };
 }
 
-export default HomePage;
+export default index
+
