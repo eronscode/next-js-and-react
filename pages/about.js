b802e74c574f88a945cc3f3ceec97f8f197d4e15
Dev_Ose <osemum@hardcorebiometric.com>
2021-04-11 13:35:18 +0100

    added second page structure

---
+++ pages/about.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function About(props) {
+    return (
+        <div>
+            <h1>About Page</h1> 
+        </div>
+    )
+}
+
+About.propTypes = {
+
+}
+
+export default About
+
