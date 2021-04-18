9af49f633141dfc155779f1392bf75ffc3a57b22
eronscode <eronscode@gmail.com>
2021-04-18 10:23:53 +0100

    Added more error handling with try and catch on the [eventid] page

--- pages/api/comments/[eventid].js
+++ pages/api/comments/[eventid].js
@@ -1,10 +1,38 @@
 import {MongoClient} from 'mongodb'
 
+async function connectDatabase() {
+     const client = await MongoClient.connect('mongodb+srv://devose:LxQn52CqH5LezlL9@cluster0.hosmk.mongodb.net/eventsdb?retryWrites=true&w=majority')
+     return client
+}
 
+async function insertDocument(client, document) {
+    const db = client.db();
+    const result = await db.collection('comments').insertOne(document);
+    return result;
+}
+
+async function getDocuments(client) {
+    const db = client.db();
+    const documents = await db.collection('comments')
+    .find()
+    .sort({_id: -1})
+    .toArray();
+
+    return documents
+}
 
 async function handler(req, res) {
     const eventID = req.query.eventid;
-    const client = await MongoClient.connect('mongodb+srv://devose:LxQn52CqH5LezlL9@cluster0.hosmk.mongodb.net/eventsdb?retryWrites=true&w=majority')
+
+    let client;
+
+    try {
+        client = await connectDatabase();
+    } catch (error) {
+        res.status(500).json({message:'Unable to connect to database'});
+        return
+    }
+    
     
 
     if(req.method === 'POST'){
@@ -13,27 +41,29 @@ async function handler(req, res) {
             res.status(422).json({message:'Invalid Input'});
             return;
         }
-
         const newComment ={
             email,
             name,
             text,
             eventID
         }
-        const db = client.db();
-        const result = await db.collection('comments').insertOne(newComment);
-        newComment.id = result.insertedId;
-        res.status(201).json({message:'Added Comment', comment: newComment})
-         
+        try {
+            const result = insertDocument(client, newComment)
+            newComment._id = result.insertedId;
+            res.status(201).json({message:'Added Comment', comment: newComment})
+        } catch (error) {
+            res.status(500).json({message:'Unable to add comment'});
+        }   
     }
 
     if(req.method === 'GET'){
-        const db = client.db();
-        const result = await db.collection('comments')
-        .find()
-        .sort({_id: -1})
-        .toArray();
-        res.status(201).json({comments: result})
+        try {
+            const result = await getDocuments(client)
+            res.status(201).json({comments: result})
+        } catch (error) {
+            console.log(error)
+            res.status(500).json({message:'Unable to fetch comments'});
+        } 
     }
 
     client.close();
