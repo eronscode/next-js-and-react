b2b1ceba6d35b06504afc319c6e247e798c8c8ae
eronscode <eronscode@gmail.com>
2021-04-18 20:49:23 +0100

    Created Notification UI

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import Layout from '../components/layout/layout'
+import Notification from '../components/ui/Notification'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <Layout>
       <Component {...pageProps} />
+      <Notification
+        title=""
+        status=""
+        message=""
+      />
   </Layout>
 }
 
